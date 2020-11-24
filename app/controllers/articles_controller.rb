2063a41715d6ad0939fc1b4b4e8987d65aac119e
Thibault Clerc <thibault.clerc2@gmail.com>
2020-11-24 17:36:13 +0100

    First commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,61 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :delete, :create]
+  before_action :set_article, only: [:show, :update, :destroy]
+
+  # GET /articles
+  def index
+    @articles = Article.all
+
+    render json: @articles
+  end
+
+  # GET /articles/1
+  def show
+    render json: @article
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+    @article.user_id = current_user.id
+
+    if @article.save
+      render json: @article, status: :created, location: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  def update
+    if article_params[user_id] != current_user.id
+      render json: @article.errors, status: :unprocessable_entity
+    else
+      if @article.update(article_params)
+        render json: @article
+      else
+        render json: @article.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  # DELETE /articles/1
+  def destroy
+    if article_params[user_id] != current_user.id
+      render json: @article.errors, status: :unprocessable_entity
+    else
+      @article.destroy
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def article_params
+      params.require(:article).permit(:title, :content, :user_id)
+    end
+end
